b1ea80204804146145a516bc57fd05ed45889dd4
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-17 11:32:28 +0100

    Projektdateien hinzufügen.

---
+++ Csharp_2021_TexteEinfachLesenUndSchreiben/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Csharp_2021_TexteEinfachLesenUndSchreiben
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnLesen = new System.Windows.Forms.Button();
+			this.btnSchreiben = new System.Windows.Forms.Button();
+			this.btnLöschen = new System.Windows.Forms.Button();
+			this.txtEinUndAusgabe = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnLesen
+			// 
+			this.btnLesen.Location = new System.Drawing.Point(13, 13);
+			this.btnLesen.Name = "btnLesen";
+			this.btnLesen.Size = new System.Drawing.Size(75, 23);
+			this.btnLesen.TabIndex = 0;
+			this.btnLesen.Text = "Lesen";
+			this.btnLesen.UseVisualStyleBackColor = true;
+			this.btnLesen.Click += new System.EventHandler(this.btnLesen_Click);
+			// 
+			// btnSchreiben
+			// 
+			this.btnSchreiben.Location = new System.Drawing.Point(95, 12);
+			this.btnSchreiben.Name = "btnSchreiben";
+			this.btnSchreiben.Size = new System.Drawing.Size(75, 23);
+			this.btnSchreiben.TabIndex = 1;
+			this.btnSchreiben.Text = "Schreiben";
+			this.btnSchreiben.UseVisualStyleBackColor = true;
+			this.btnSchreiben.Click += new System.EventHandler(this.btnSchreiben_Click);
+			// 
+			// btnLöschen
+			// 
+			this.btnLöschen.Location = new System.Drawing.Point(177, 13);
+			this.btnLöschen.Name = "btnLöschen";
+			this.btnLöschen.Size = new System.Drawing.Size(75, 23);
+			this.btnLöschen.TabIndex = 2;
+			this.btnLöschen.Text = "Löschen";
+			this.btnLöschen.UseVisualStyleBackColor = true;
+			this.btnLöschen.Click += new System.EventHandler(this.btnLöschen_Click);
+			// 
+			// txtEinUndAusgabe
+			// 
+			this.txtEinUndAusgabe.Location = new System.Drawing.Point(12, 42);
+			this.txtEinUndAusgabe.Multiline = true;
+			this.txtEinUndAusgabe.Name = "txtEinUndAusgabe";
+			this.txtEinUndAusgabe.Size = new System.Drawing.Size(240, 197);
+			this.txtEinUndAusgabe.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(276, 260);
+			this.Controls.Add(this.txtEinUndAusgabe);
+			this.Controls.Add(this.btnLöschen);
+			this.Controls.Add(this.btnSchreiben);
+			this.Controls.Add(this.btnLesen);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnLesen;
+		private System.Windows.Forms.Button btnSchreiben;
+		private System.Windows.Forms.Button btnLöschen;
+		private System.Windows.Forms.TextBox txtEinUndAusgabe;
+	}
+}
+
---
+++ Csharp_2021_TexteEinfachLesenUndSchreiben/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_2021_TexteEinfachLesenUndSchreiben
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnLöschen_Click(object sender, EventArgs e)
+		{
+			txtEinUndAusgabe.Text = "";
+		}
+
+		private void btnSchreiben_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				// SaveFileDialog
+				var sfd = new SaveFileDialog()
+				{
+					InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+					Filter = "Textdateien (*.txt)|*.txt|" +
+					"Alle Dateien (*.*)|*.*",
+					Title = "Datei speichern unter..."
+				};
+
+				//// Prüfen, ob Button "Speichern" gedrückt wurde, andernfalls Abbruch
+				//if (sfd.ShowDialog() != DialogResult.OK) return;
+
+				//// Text in die Datei schreiben (Datei wird überschrieben)
+				//File.WriteAllText(sfd.FileName, txtEinUndAusgabe.Text);
+
+				// Prüfen, ob Button "Speichern" gedrückt wurde 
+				// => Nur dann wird der Text in die Datei geschrieben
+				if (sfd.ShowDialog() == DialogResult.OK) 
+					File.WriteAllText(sfd.FileName, txtEinUndAusgabe.Text);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void btnLesen_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				// OpenFileDialog
+				var ofd = new OpenFileDialog()
+				{
+					InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+					Filter = "Textdateien (*.txt)|*.txt|" +
+						"Alle Dateien (*.*)|*.*",
+					Title = "Datei öffnen..."
+				};
+
+				if (ofd.ShowDialog() == DialogResult.OK)
+					txtEinUndAusgabe.Text = File.ReadAllText(ofd.FileName);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
+}
